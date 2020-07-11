0739c12653b7bcf8526e0f2c04d6e5cdb9979462
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-11 16:55:04 +0100

    appState set for paused & active/background

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, AppState } from 'react-native'
 import homePg from './HomeView.styles'
 import i18n from '../../i18n/i18n';
 import StopWatchBtn from '../stopWatchBtn/StopWatchBtn';
+import AsyncStorage from '@react-native-community/async-storage'
 export default class HomeView extends Component {
   constructor(props){
     super(props)
@@ -11,9 +12,44 @@ export default class HomeView extends Component {
     }
     this.startTimer = this.startTimer.bind(this);
     this.pausedTimer = this.pausedTimer.bind(this);
+    this.handleAppStateChange = this.handleAppStateChange.bind(this);
   }
+  componentDidMount(){
+    AppState.addEventListener('change' , this.handleAppStateChange)
+  }
+  componentWillUnmount(){
+    AppState.removeEventListener('change', this.handleAppStateChange)
+  }
+  async handleAppStateChange(nextAppState) {
+    const now = new Date().getTime();
+    const {time, paused} = this.state;
+    const readTime = AsyncStorage.getItem('@time')
+    // time when you went off
+    const readStateChangeTimeStamp = AsyncStorage.getItem('@appStateChangedTimeStamp')
+    
+    const timeDiff = now - parseInt(readStateChangeTimeStamp)
+    const nueTime = parseInt(readTime) + timeDiff
+    
+    if(nextAppState === 'active'){
+      const isPaused = await AsyncStorage.getItem('@isPaused')
+      const wasPaused = isPaused && isPaused ==='true'
+      let newState = {
+        pauded: wasPaused,
+        time: parseInt(readTime)
+      }
+      if(!wasPaused){
+        newState.time = nueTime
+      }
+      this.setState(newState , this.startTimer)
+    }
+    await AsyncStorage.setItem('@isPaused', paused === true ? true : false)
+    await AsyncStorage.setItem('@time', time)
+    await AsyncStorage.setItem('@appStateChangegTimeStamp', now)
+  }
+
   startTimer(){
-    setInterval(() => {
+    if(this.timerIntervalId) clearInterval(this.timerIntervalId)
+    this.timerIntervalId = setInterval(() => {
       const {time,paused}= this.state;
       if(!paused){
         this.setState({time: time + 1000})
