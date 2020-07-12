baf1939b5d8b502ec5e696831399246d26d9cc36
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-12 21:24:23 +0100

    FinishView & HistoryView in progress

--- src/components/layout/actionBtn/ActionBtn.js
+++ src/components/layout/actionBtn/ActionBtn.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { TouchableOpacity, Text, StyleSheet } from 'react-native'
 
-const ActionBtn = ({label, textColor, backgroundColor,onPress}) => {
-  return <TouchableOpacity 
-    style={{ width: 134,height:34,borderRadius: 15, backgroundColor}}
-    onPress={onPress}
-  >
+const ActionBtn = ({label, textColor, backgroundColor}) => {
+  return <TouchableOpacity style={[styles.btn , {backgroundColor}]}>
   <Text style={{ color: textColor }}>{label}</Text>
 </TouchableOpacity>
 }
 
+const styles = StyleSheet.create({
+  btn:{
+    width: 134,
+    height:34,
+    borderRadius: 15,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  captionStyle:{
+    
+  }
+})
 export default ActionBtn
