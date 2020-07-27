2e78595bc7a6063782b59b1a9122fa96d4ca27e0
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-07-27 21:04:52 +0100

    App & app.json in progress

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ export default function App() {
           }
         })}
         tabBarOptions={{
-          activeTintColor: ' rgba(0, 0, 0, 0.4)',
+          activeTintColor: 'rgba(0, 0, 0, 0.4)',
           inactiveTintColor: 'rgba(255, 255, 255, 0.9)',
           opacity: 0.8,
           lazyLoad:true,
